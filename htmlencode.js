af4b424f8dde1d7ab6fc08418309ad5499d2ebb7
Hoa V. Dinh <dvh@google.com>
2013-05-06 20:44:33 -0700

    Added missing file

---
+++ htmlencode.js
@@ -0,0 +1,9 @@
+function htmlEncode(value){
+  //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+  //then grab the encoded contents back out.  The div never exists on the page.
+  return $('<div/>').text(value).html();
+}
+
+function htmlDecode(value){
+  return $('<div/>').html(value).text();
+}
