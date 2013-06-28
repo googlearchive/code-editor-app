912460e6d9cc75634bc723f435f91f2aa247783a
Hoa V. Dinh <dvh@google.com>
2013-06-28 16:53:08 -0700

    Cleanup comments.

--- buffer.js
+++ buffer.js
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//var openedTabEntries = new Array();
-//var openedTabHash = new Object();
-
 Buffer = function(fileEntry, spark) {
   this.fileEntry = fileEntry;
   this.spark = spark;
